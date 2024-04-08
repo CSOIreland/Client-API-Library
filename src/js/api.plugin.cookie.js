5f1a47bca3af0f9699e145783c2cf43af761a783
Stephen Lane <stephen.lane@cso.ie>
2024-04-08 16:06:54 +0100

    non json cookie bug fix

--- src/js/api.plugin.cookie.js
+++ src/js/api.plugin.cookie.js
@@ -6,11 +6,17 @@ Cookies.setJSON = function (cookieName, data, cookieOptions) {
     Cookies.set(cookieName, JSON.stringify(data), cookieOptions);
 }
 
-
 Cookies.getJSON = function (cookieName) {
     var cookieValue = Cookies.get(cookieName);
     if (cookieValue) {
-        return JSON.parse(cookieValue)
+        try {
+            return JSON.parse(cookieValue)
+        } catch (error) {
+            Cookies.remove(cookieName);
+            console.warn(["invalid cookie removed", cookieName, error])
+            return null;
+        }
+
     }
     else {
         return cookieValue
